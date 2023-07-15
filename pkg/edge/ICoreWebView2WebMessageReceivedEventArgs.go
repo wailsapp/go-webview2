5e5b7d68c0b3e7f2380aea62d4c78944dda793b6
Hidayatullah <ayat.kyo@gmail.com>
2023-07-15 22:31:02 +0800

    chore: implement GetAdditionalObjects
    Signed-off-by: Hidayatullah <ayat.kyo@gmail.com>

--- pkg/edge/ICoreWebView2WebMessageReceivedEventArgs.go
+++ pkg/edge/ICoreWebView2WebMessageReceivedEventArgs.go
@@ -2,13 +2,33 @@
 
 package edge
 
+import (
+	"unsafe"
+
+	"golang.org/x/sys/windows"
+)
+
 type iCoreWebView2WebMessageReceivedEventArgsVtbl struct {
 	_IUnknownVtbl
 	GetSource                ComProc
 	GetWebMessageAsJSON      ComProc
 	TryGetWebMessageAsString ComProc
+	GetAdditionalObjects     ComProc
 }
 
 type iCoreWebView2WebMessageReceivedEventArgs struct {
 	vtbl *iCoreWebView2WebMessageReceivedEventArgsVtbl
+}
+
+func (i *iCoreWebView2WebMessageReceivedEventArgs) GetAdditionalObjects() (*ICoreWebView2ObjectCollectionView, error) {
+	var err error
+	var value *ICoreWebView2ObjectCollectionView
+	_, _, err = i.vtbl.GetAdditionalObjects.Call(
+		uintptr(unsafe.Pointer(i)),
+		uintptr(unsafe.Pointer(&value)),
+	)
+	if err != windows.ERROR_SUCCESS {
+		return nil, err
+	}
+	return value, nil
 }
