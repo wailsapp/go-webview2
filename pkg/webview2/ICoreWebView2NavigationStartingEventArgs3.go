ad6d8723f1d4b39054350c7f0b8955b845f44411
Lea Anthony <lea.anthony@gmail.com>
2023-07-26 08:40:13 +1000

    Update to 1.0.1901.177

---
+++ pkg/webview2/ICoreWebView2NavigationStartingEventArgs3.go
@@ -0,0 +1,49 @@
+//go:build windows
+
+package webview2
+
+import (
+	"golang.org/x/sys/windows"
+	"syscall"
+	"unsafe"
+)
+
+type ICoreWebView2NavigationStartingEventArgs3Vtbl struct {
+	IUnknownVtbl
+	GetNavigationKind ComProc
+}
+
+type ICoreWebView2NavigationStartingEventArgs3 struct {
+	Vtbl *ICoreWebView2NavigationStartingEventArgs3Vtbl
+}
+
+func (i *ICoreWebView2NavigationStartingEventArgs3) AddRef() uintptr {
+	refCounter, _, _ := i.Vtbl.AddRef.Call(uintptr(unsafe.Pointer(i)))
+	return refCounter
+}
+
+func (i *ICoreWebView2) GetICoreWebView2NavigationStartingEventArgs3() *ICoreWebView2NavigationStartingEventArgs3 {
+	var result *ICoreWebView2NavigationStartingEventArgs3
+
+	iidICoreWebView2NavigationStartingEventArgs3 := NewGUID("{DDFFE494-4942-4BD2-AB73-35B8FF40E19F}")
+	_, _, _ = i.Vtbl.QueryInterface.Call(
+		uintptr(unsafe.Pointer(i)),
+		uintptr(unsafe.Pointer(iidICoreWebView2NavigationStartingEventArgs3)),
+		uintptr(unsafe.Pointer(&result)))
+
+	return result
+}
+
+func (i *ICoreWebView2NavigationStartingEventArgs3) GetNavigationKind() (*COREWEBVIEW2_NAVIGATION_KIND, error) {
+
+	var navigation_kind COREWEBVIEW2_NAVIGATION_KIND
+
+	hr, _, err := i.Vtbl.GetNavigationKind.Call(
+		uintptr(unsafe.Pointer(i)),
+		uintptr(unsafe.Pointer(&navigation_kind)),
+	)
+	if windows.Handle(hr) != windows.S_OK {
+		return nil, syscall.Errno(hr)
+	}
+	return &navigation_kind, err
+}
